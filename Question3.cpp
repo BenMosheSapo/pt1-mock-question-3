cc7e1510bfd842e39313b19808b3a2c7fb3fc241
Ben Sapo <bensapo@gmail.com>
2016-03-21 13:34:24 +0200

    Overloaded > operator. Overloaded 2 add functions. Added testing

--- Question3.cpp
+++ Question3.cpp
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //                         COMPUTER METHODS 3 PRACTICAL TEST 1 (MOCK)
-// Name:
-// Student No:
-// Date: 
+// Name: Ben Moshe Sapo
+// Student No: 214527679
+// Date: 21/03/2016
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 
@@ -22,6 +22,41 @@ private:
 	int num;				// numerator;
 	int denom;				// denominator;
 public:
-	Fraction(int n, int d) : num(n), denum(d) { };
+	Fraction(int n, int d) : num(n), denom(d) { };
 	void print() { cout << num << "/" << denom; };
+	friend int operator > (Fraction frac1, Fraction frac2);
+	friend Fraction add(int whole, Fraction frac);
+	friend Fraction add(Fraction frac, int whole);
 };
+
+//Multiplies whole number by denominator to give it a common denominator then adds it to the numerator
+Fraction add(int whole, Fraction frac) {
+	Fraction Result((whole * frac.denom) + frac.num, frac.denom);
+	return Result;
+}
+Fraction add(Fraction frac, int whole) {
+	return add(whole, frac);
+}
+
+//Cross multiplies to create common denominator then compares numerator
+int operator > (Fraction frac1, Fraction frac2) {
+	int num1, num2;
+	num1 = frac1.num * frac2.denom;
+	num2 = frac2.num * frac1.denom;
+	if (num1 > num2)
+		return 1;
+	else
+		return 0;
+}
+
+int main() {
+	Fraction frac1(8,10);
+	Fraction frac2(3, 4);
+	if (frac1 > frac2)
+		cout << "It is greater" << endl;
+	Fraction Result	= add(1, frac1);
+	Result.print();
+	cout << endl;
+	Result = add(frac2, 1);
+	Result.print();
+}
